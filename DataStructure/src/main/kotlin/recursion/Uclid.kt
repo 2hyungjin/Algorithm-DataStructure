1dd5494eab4e259555ee4329ce6c4269f9ed571a
2hyungjin <63038103+2hyungjin@users.noreply.github.com>
2021-11-04 22:53:45 +0900

    최대공약수 최소공배수

--- DataStructure/src/main/kotlin/recursion/Uclid.kt
+++ DataStructure/src/main/kotlin/recursion/Uclid.kt
@@ -1,6 +1,4 @@
 package recursion
 
 fun getGcd(x: Int, y: Int): Int = if (x % y == 0) y else getGcd(y, x % y)
-fun main() {
-    println(getGcd(8,22))
-}
+fun getLcm(x: Int, y: Int) = x * y / getGcd(x, y)
