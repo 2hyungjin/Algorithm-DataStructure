76503b1e30c412dcecb258c1236e84fd7babc708
2hyungjin <63038103+2hyungjin@users.noreply.github.com>
2021-11-15 19:50:18 +0900

    8퀸

---
+++ DataStructure/src/main/kotlin/recursion/EightQueen.kt
@@ -0,0 +1,35 @@
+package recursion
+
+fun getEightQueen(column: Int = 8) {
+    var answer = 0
+
+
+    fun setQueen(n: Int, FLAG_COL: BooleanArray, FLAG_CROSS_LEFT: BooleanArray, FLAG_CROSS_RIGHT: BooleanArray) {
+        for (i in 0 until column) {
+            if (FLAG_COL[i] && FLAG_CROSS_LEFT[i + n] && FLAG_CROSS_RIGHT[i - n + 7]) {
+
+                FLAG_COL[i] = false
+                FLAG_CROSS_LEFT[i + n] = false
+                FLAG_CROSS_RIGHT[i - n + 7] = false
+
+                if (n == 7) answer++
+                else setQueen(n + 1, FLAG_COL, FLAG_CROSS_LEFT, FLAG_CROSS_RIGHT)
+
+                FLAG_COL[i] = true
+                FLAG_CROSS_LEFT[i + n] = true
+                FLAG_CROSS_RIGHT[i - n + 7] = true
+            }
+        }
+
+    }
+
+    setQueen(0, BooleanArray(8) { true }, BooleanArray(15) { true }, BooleanArray(15) { true })
+    println(answer)
+
+}
+
+
+fun main() {
+    getEightQueen()
+}
+
